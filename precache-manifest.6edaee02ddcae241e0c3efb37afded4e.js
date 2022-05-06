1155e3aa770b6b198303405a0441a6aebb6c0cc3
suguna <sugunamc0@gmail.com>
2022-05-06 16:14:26 +0530

    Updates

---
+++ precache-manifest.6edaee02ddcae241e0c3efb37afded4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bf3b90c6c507cb22dcfdf3a0b47340",
+    "url": "/spotify_app/index.html"
+  },
+  {
+    "revision": "4fe8acc8f2d5fdc9f12e",
+    "url": "/spotify_app/static/css/main.f607e9ea.chunk.css"
+  },
+  {
+    "revision": "aa5297dcfbf498dd1078",
+    "url": "/spotify_app/static/js/2.eced1c94.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify_app/static/js/2.eced1c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fe8acc8f2d5fdc9f12e",
+    "url": "/spotify_app/static/js/main.92451a33.chunk.js"
+  },
+  {
+    "revision": "bc531031675cbf5baee2",
+    "url": "/spotify_app/static/js/runtime-main.cb9b018d.js"
+  }
+]);
